c1f3d7cccc061b89bd6e0a8a4b8ada0822bac4b5
hrzhao76 <hrzhao76@gmail.com>
2026-01-06 23:15:39 -0500

    feat: init nesap seminar talk

---
+++ NESAP-Workflow-Postdoc/global-bottom.vue
@@ -0,0 +1,29 @@
+<!-- global-bottom.vue -->
+<template>
+  <footer
+    v-if="$slidev.nav.currentLayout !== 'cover'
+      && $slidev.nav.currentLayout !== 'center'
+      && $slidev.nav.currentPage !== $slidev.nav.total"
+    class="absolute bottom-0 left-0 w-full px-4 py-1 text-gray-600"
+  >
+    <div class="grid grid-cols-3 items-center text-sm">
+      <!-- 左 -->
+      <div class="text-left">
+        Haoran Zhao
+      </div>
+
+      <!-- 中 -->
+      <div class="text-center">
+        Seminar
+      </div>
+
+      <!-- 右 -->
+      <div class="text-right">
+        {{ $slidev.nav.currentPage }}
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script setup lang="ts">
+</script>
